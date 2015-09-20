20d4ec1b2a9b43336196c1dfe77505944f77a45a
ssyqifa <sunqifa@live.com>
2015-09-20 17:24:46 +0800

    编写Delete List

--- DSAAinC/实现.h
+++ DSAAinC/实现.h
@@ -1,4 +1,17 @@
+#include <stdlib.h>
+#include "List.h"
+
+using namespace std;
 void DeleteList(List L)
 {
-
+	Position P, tmp;
+	
+	P = L->Next;
+	L->Next = NULL;
+	while (P != NULL)
+	{
+		tmp = P->Next;
+		free(P);
+		P = tmp;
+	}
 }
