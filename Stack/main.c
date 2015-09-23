d0da82ad1d50fc795622b4b91f6f52cdde60372a
ssyqifa <sunqifa@live.com>
2015-09-23 09:45:46 +0800

    添加主函数输出的提示信息

--- Stack/main.c
+++ Stack/main.c
@@ -6,7 +6,18 @@ void main()
 	Stack S=CreatStack();
 	MakeEmpty(S);
 	Pop(S);
+	printf("\n");
 	Push(1.2, S);
-	printf("%f", Top(S));
+	printf("push(1.2),top(s):%f\n", Top(S));
+	Push(2, S);
+	printf("push(2),top(s):%f\n", Top(S));
+	Push(3, S);
+	printf("push(3),top(s):%f\n", Top(S));
+	Push(4.3, S);
+	printf("push(4),top(s):%f\n", Top(S));
+	Pop(S);
+	printf("pop(s),top(s):%f\n", Top(S));
+	Pop(S);
+	printf("pop(s),top(s):%f\n", Top(S));
 
 }
